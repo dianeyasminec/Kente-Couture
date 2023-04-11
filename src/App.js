9865f22de3b5a88089a0cfff45235a4cada362eb
Diane <dianeyasminec60@gmail.com>
2023-04-11 12:31:01 -0400

    Process

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
      <Home/>
+     <Footer/>
     </div>
   );
 }
