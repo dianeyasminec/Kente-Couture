8e381d16cc933b44b39a70631da7e6c0cd78a7b7
Diane <dianeyasminec60@gmail.com>
2023-04-11 12:43:57 -0400

    Process

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home";
+import {
+  createBrowserRouter,
+     Route,
+  createRoutesFromElements,
+  RouterProvider} 
+  from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
+import Layout from './components/Layout';
+// import About from './components/About'
+// import AllRecipes from './components/AllRecipes';
+// import Reviews from './components/Reviews';
+// import HelpLayout from './components/HelpLayout';
+// import Faq from './components/Faq';
+// import Contact from './components/Contact';
+// import NotFound from './components/NotFound';
+// import ScrollToTop from './components/ScrollToTop';
+// import details from './data/details'
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+          <Route index element={<Home/>}/>
+          {/* <Route path='allrecipes'  element={ <AllRecipes details={details}/>}/>
+          <Route path='reviews'  element={ <Reviews />}/> */}
+          
+          {/* Nested Routes */}
+
+          {/* <Route path='help' element={<HelpLayout/>}>
+            <Route path='faq' element={<Faq/>}/>
+            <Route path='contact' element={<Contact/>}/>
+          </Route>
+          <Route path="*" element={<NotFound />} /> */}
+      </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-     <Home/>
-     <Footer/>
-    </div>
+    <>
+      <RouterProvider router={router}/>
+        
+  {/* <ScrollToTop /> */}
+  <Footer className='m-3'/>
+    </>
   );
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import {Nav} from 'react-bootstrap'
+
+export default function Layout() {
+  return (
+    <div className='root-layout'>
+      <header>
+        <nav>
+          <Navbar.Brand>Kente Couture</Navbar.Brand>
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '150px' }}
+            navbarScroll
+          >
+            <NavLink to="/" style={{ marginRight: '150px' }}>Home</NavLink>
+            {/* <NavLink to="Home" style={{ marginRight: '150px' }}>All Recipes</NavLink> */}
+            {/* <NavLink to="reviews" style={{ marginRight: '150px' }}>Reviews</NavLink>
+            <NavLink to="help" style={{ marginRight: '20px' }}>Help</NavLink> */}
+          </Nav>
+        </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
+  )
+}
